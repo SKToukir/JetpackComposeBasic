820044846f6f6c76e9f27bd8dca9318f1b3cc423
SKToukir <toukirsk@gmail.com>
2024-10-21 16:08:13 +0600

    add statusbar safe padding

--- app/src/main/java/com/walton/androidarchitecture/MainActivity.kt
+++ app/src/main/java/com/walton/androidarchitecture/MainActivity.kt
@@ -16,11 +16,14 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -296,7 +299,7 @@ fun CardContent(name: String) {
 
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
-
+    
     var count by remember {
         mutableStateOf(0)
     }
@@ -308,7 +311,7 @@ fun MyApp(modifier: Modifier = Modifier) {
     }
 
     Surface(
-        modifier = modifier, color = MaterialTheme.colorScheme.background
+        modifier = modifier.padding(WindowInsets.statusBars.asPaddingValues()), color = MaterialTheme.colorScheme.background
     ) {
         if (shouldShowOnBoarding) {
             OnBoardingScreen(OnContinueClicked = {
